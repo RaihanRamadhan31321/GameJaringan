2962b4f7afd9ed01f49699bb46742bf944582b35
RaihanRamadhan31321 <144348683+RaihanRamadhan31321@users.noreply.github.com>
2024-11-26 14:29:54 +0700

    Fix bug Main menu

--- ProjectGJ/GameJaringan/Assets/Script/script.cs
+++ ProjectGJ/GameJaringan/Assets/Script/script.cs
@@ -39,7 +39,6 @@ public class ManajerJaringan : MonoBehaviourPunCallbacks
         ActivatePanel(PanelLogin.name);
         cachedDaftarRoom = new Dictionary<string, RoomInfo>();
         daftarRoomGameObjects = new Dictionary<string, GameObject>();
-        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     // Tombol Login
@@ -60,22 +59,35 @@ public class ManajerJaringan : MonoBehaviourPunCallbacks
         }
     }
 
-    // Tombol buat room
+    //Tombol buat room
     public void OnRoomCreateButtonClicked()
     {
         string NamaRoom = NamaRoomInputField.text;
+        string maxPlayer = maxPlayerInputField.text;
 
         if (string.IsNullOrEmpty(NamaRoom))
         {
             NamaRoom = "Room " + Random.Range(1000, 10000);
+            Debug.Log("Nama Room wajib di isi");
+            NamaRoomInputField.placeholder.GetComponent<Text>().text = "Nama Tidak Boleh Kosong!";
+            NamaRoomInputField.placeholder.GetComponent<Text>().color = Color.red;
+        }
+        else if (string.IsNullOrEmpty(maxPlayer))
+        {
+            Debug.Log("index wajib di isi");
+            maxPlayerInputField.placeholder.GetComponent<Text>().text = "index Tidak Boleh Kosong!";
+            maxPlayerInputField.placeholder.GetComponent<Text>().color = Color.red;
         }
 
-        RoomOptions roomOptions = new RoomOptions
-        {
-            MaxPlayers = (byte)int.Parse(maxPlayerInputField.text)
-        };
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = (byte)int.Parse(maxPlayerInputField.text);
 
         PhotonNetwork.CreateRoom(NamaRoom, roomOptions);
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("Terhubung ke Internet");
     }
 
     public override void OnConnectedToMaster()
@@ -110,7 +122,7 @@ public class ManajerJaringan : MonoBehaviourPunCallbacks
 
             daftarEntriRoomGameobject.transform.Find("NamaRoomText").GetComponent<Text>().text = room.Name;
             daftarEntriRoomGameobject.transform.Find("MaksPlayerText").GetComponent<Text>().text =
-                room.PlayerCount + " /10" + room.MaxPlayers;
+                room.PlayerCount + " /" + room.MaxPlayers;
 
             daftarEntriRoomGameobject.transform.Find("TombolLihatDetailRoom").GetComponent<Button>()
                 .onClick.AddListener(() => OnShowJoinRoomPanel(room.Name));
@@ -137,12 +149,12 @@ public class ManajerJaringan : MonoBehaviourPunCallbacks
         if (roomInfo != null)
         {
             namaRoomJoinText.text = "Room: " + roomName;
-            infoPlayerJoinText.text = "Players: " + roomInfo.PlayerCount + " /10" + roomInfo.MaxPlayers;
+            infoPlayerJoinText.text = "Players: " + roomInfo.PlayerCount + " /" + roomInfo.MaxPlayers;
         }
         else
         {
             namaRoomJoinText.text = "Room: " + roomName;
-            infoPlayerJoinText.text = "Players:  /10";
+            infoPlayerJoinText.text = "Players:  /";
         }
 
         // Aktifkan tombol Mulai jika pemain adalah Master Client
@@ -163,20 +175,7 @@ public class ManajerJaringan : MonoBehaviourPunCallbacks
 
     public void OnStartGameButtonClicked()
     {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            if (!PhotonNetwork.IsMessageQueueRunning)
-            {
-                Debug.LogWarning("Photon message queue is paused!");
-                return;
-            }
-
-            PhotonNetwork.LoadLevel("GamePlay");
-        }
-        else
-        {
-            Debug.LogWarning("Hanya Master Client yang dapat memulai permainan!");
-        }
+        PhotonNetwork.LoadLevel("GamePlay");
     }
 
     public override void OnCreatedRoom()
